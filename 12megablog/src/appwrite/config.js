78426f4947d8acfbdab34ad5179439170f558719
Piyush Garg <piyushgarg809@gmail.com>
2025-01-13 22:03:03 +0530

    appwrite service is ready

--- 12megablog/src/appwrite/config.js
+++ 12megablog/src/appwrite/config.js
@@ -33,10 +33,101 @@ export class Service{
         }
     }
 
-    async updatePost(){
-        
+    async updatePost( slug , {title , content , featuredImage , status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId , 
+                conf.appwriteCollectionId , 
+                slug , 
+                {
+                    title , 
+                    content , 
+                    featuredImage , 
+                    status , 
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: update post :: error :" + error );
+            
+        }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId , 
+                conf.appwriteCollectionId , 
+                slug , 
+            )
+            return true ; 
+        } catch (error) {
+            console.log("Appwrite service :: delete Post :: error :" + error );
+             return false ; 
+        }
+    }
+    
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId , 
+                conf.appwriteCollectionId , 
+                slug , 
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error :" + error );
+             return false ;            
+        }
     }
 
+    async getPosts(queries = [Query.equal("status" , "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId , 
+                conf.appwriteCollectionId , 
+                queries , 
+
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error :" + error );
+            return false ; 
+        }
+    }
+
+    //upload file to appwrite 
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId , 
+                Id.unique() , 
+               file , 
+            )
+        } catch (error) {
+            console.log("Appwrite service :: upload file :: error :" , error );
+            return false ; 
+            
+        }
+    }
+
+    async deleteFile(fileId){
+         try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId , 
+                fileId ,  
+               )
+               return true ; 
+         } catch (error) {
+            console.log("Appwrite service :: delete file :: error :" , error );
+            return false ; 
+            
+         }
+      }
+
+      async getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId , 
+            fileId , 
+        )
+      }
 }
 
 const service = new Service() ; 
