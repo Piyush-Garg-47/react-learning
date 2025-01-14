24d00b01bf5ff7caa7e6a1efec6b2b13b25d997e
Piyush Garg <piyushgarg809@gmail.com>
2025-01-14 15:35:25 +0530

    header footer complete

--- 12megablog/src/App.jsx
+++ 12megablog/src/App.jsx
@@ -1,14 +1,38 @@
-
+import { useState , useEffect } from 'react'
+import {useDispatch} from 'react-redux' ; 
 import './App.css'
+import authService from './appwrite/auth'
+import {login , logout} from './authSlice'
+import { Footer, Header } from './components';
 
 function App() {
+    const[loading , setLoading] = useState(true) ; 
+    const dispatch = useDispatch() ; 
+    
+    useEffect(()=>{
+      authService.getCurrentUser.then( (userData) =>{
+           if(userData){
+            dispatch(login({userData}))
+           }else{
+              dispatch(logout())
+           }
+      })
+      .finally(()=>setLoading(false))
+    } , [])
 
-   console.log(import.meta.env.VITE_APPWRITE_URL) ; 
-  return (
-    <>
-     <h1>A blog with appworte </h1>
-    </>
-  )
+
+
+  return  !loading ?(
+    <div className='min-h-screen flex flex-wrap bg-green-400 '>
+      <div className='w-full block '>
+         <Header />
+           <main>
+            {/* <Outlet /> */}
+           </main>
+         <Footer />
+      </div>
+    </div>
+  ) : null 
 }
 
 export default App
--- 12megablog/src/appwrite/auth.js
+++ 12megablog/src/appwrite/auth.js
@@ -1,5 +1,5 @@
-import conf from "../conf/conf";
-import {Client , Id, Account } from "appwrite";
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
       client = new Client() ; 
